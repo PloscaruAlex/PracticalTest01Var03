32b899b7cc86fe97278ab672695292037759f9bf
PloscaruAlex <ploscarualexandru25@gmail.com>
2025-11-10 10:27:41 +0200

    Created Android Project

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.kt
@@ -0,0 +1,11 @@
+package ro.pub.cs.systems.eim.practicaltest01var03
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class PracticalTest01Var03MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practical_test01_var03_main)
+    }
+}
